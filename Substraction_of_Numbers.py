e8b993346725f449520892e1f21cf1eaf66b5575
Xion <83879915+devXION@users.noreply.github.com>
2021-08-09 16:01:57 +0200

    Add files via upload
    Substraction of Numbers.

---
+++ Substraction_of_Numbers.py
@@ -0,0 +1,15 @@
+#as previously we declare intended numbers in form of num
+num1= 8
+num2= 2
+#substraction of num
+sub = num1 - num2
+print('Substraction of {0} and {1} is {2}'.format(num1, num2, sub))
+
+#the same applies to multiple numbers
+num1= 8
+num2= 2
+num3= 3 
+num4= 1
+sub = num1 - num2 - num3 - num4 
+
+print('Substraction of {0} - {1} - {2} - {3} is {4}'.format(num1, num2, num3, num4, sub))
