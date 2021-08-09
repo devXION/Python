2196d05fd800add68ec483649700fa68d65ff2f7
Xion <83879915+devXION@users.noreply.github.com>
2021-08-09 15:37:03 +0200

    Add files via upload
    Code responsible for sum of numbers.

---
+++ Sum_of_numbers.py
@@ -0,0 +1,16 @@
+#declare your desired numbers as num
+num1= 2
+num2= 5
+#this function adds our num1 and num2
+sum = num1 + num2
+#print our result
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+#in case of more numbers we repeat the same code but with addition of num in the last line of code
+num1= 2
+num2= 5
+num3= 3
+
+sum = num1 + num2 + num3
+
+print('The sum of {0} and {1} + {2}  is {3}'.format(num1, num2, num3, sum))
